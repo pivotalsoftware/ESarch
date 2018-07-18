67659b889c0858d8e5c9f90e7edfa3da518fdfe2
smcvb <smcvbeelen@gmail.com>
2018-07-18 15:26:26 +0200

    Several minor adjustments to the properties
    -Disable usage of command router fallback
    -Rename query-side properties file to application.properties
    -Set Hibernate dialect
    -Copy over other necessary properties to query-side
    -Copy SecurityConfig into query-side

---
+++ query-side/src/main/java/io/pivotal/catalog/SecurityConfig.java
@@ -0,0 +1,17 @@
+package io.pivotal.catalog;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Order(1000)
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll();
+        super.configure(http);
+    }
+}
