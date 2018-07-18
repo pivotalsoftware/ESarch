ca24e453b37232efd37c1061b84dc23abace8ad9
smcvb <smcvbeelen@gmail.com>
2018-07-18 15:13:56 +0200

    Add Order of 1000 to SecurityConfig
    Add Order of 1000 to SecurityConfig

--- command-side/src/main/java/io/pivotal/refarch/cqrs/trader/SecurityConfig.java
+++ command-side/src/main/java/io/pivotal/refarch/cqrs/trader/SecurityConfig.java
@@ -1,9 +1,11 @@
 package io.pivotal.refarch.cqrs.trader;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Order(1000)
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
