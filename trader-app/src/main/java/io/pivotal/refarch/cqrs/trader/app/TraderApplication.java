bd83e679a99bec84a9ec31b3135c48255ec3d1f1
Allard Buijze <allard.buijze@axoniq.io>
2018-07-25 21:28:05 +0200

    Fixed security configuration

--- trader-app/src/main/java/io/pivotal/refarch/cqrs/trader/app/TraderApplication.java
+++ trader-app/src/main/java/io/pivotal/refarch/cqrs/trader/app/TraderApplication.java
@@ -10,12 +10,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 @SpringBootApplication
 public class TraderApplication {
 
@@ -34,16 +31,5 @@ public class TraderApplication {
                 return interceptorChain.proceed();
             }
         });
-    }
-
-    @Configuration
-    public static class TraderSecurity extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            // TODO: Figure this out...
-            http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/**").permitAll();
-        }
     }
 }
