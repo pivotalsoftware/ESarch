6a4fd799f46aa8365590b605e9c5fbbf3d6acbba
benwilcock <benwilcock@gmail.com>
2018-07-25 15:36:51 +0100

    Added a second smoke test for the Trader-app

--- ci/tasks/smoke-test-query-side.sh
+++ ci/tasks/smoke-test-query-side.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 
-apt-get update && apt-get install -y curl uuid --allow-unauthenticated
+apt-get update && apt-get install -y curl uuid jq --allow-unauthenticated
 
 #set -ex
 
 # Begin the Integration-testing...
 
-export UUID=`uuid`
-if curl -X POST -sL -w %{http_code} -d "${UUID}" "${URL}/company" -o /dev/null | grep "200"
+export RANDOM_NAME=`uuid`
+export COMPANY_UUID=`curl -X POST -sL -d "${RANDOM_NAME}" -H "Content-Type:application/json" ${URL}/company | jq -r .identifier`
+
+
+if [ "$COMPANY_UUID" != "" ]
 then
-    echo "[$URL] shows 'HTTP/1.1 200 OK' (as expected)."
+    echo "[$URL/company] has created a company called $RANDOM_NAME with ID $COMPANY_UUID (as expected)."
 else
-    echo -e "\e[31mError. Not showing '200 OK' on [$URL]"
+    echo -e "\e[31mError. Didn't create a new company!"
+    exit 1
+fi
+
+export COMPANY_NAME=`curl -sL -H "Content-Type: application/json" -X GET ${URL}/orderbook/${COMPANY_UUID} | jq -r '.[]|.companyName'
+if [ "$COMPANY_NAME" = "$RANDOM_NAME" ]
+then
+    echo "[$URL/orderbook/$COMPANY_UUID] Has been found (as expected)."
+else
+    echo -e "\e[31mError. Unable to find the orderbook for company [$RANDOM_NAME]"
     exit 1
 fi
 
