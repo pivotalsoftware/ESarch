af001d5c52dcd8a2e2e2fffa1d27b948a0f51218
benwilcock <benwilcock@gmail.com>
2018-07-26 10:47:52 +0100

    fixing wording in the e2e-test

--- ci/tasks/integration-test-both-sides.sh
+++ ci/tasks/integration-test-both-sides.sh
@@ -6,18 +6,18 @@ apt-get update && apt-get install -y curl uuid jq --allow-unauthenticated
 
 if [ -z $engineURL ];
 then
-  echo -e "\e[31mThe Command-side URL to test has not been set."
+  echo -e "\e[31mThe Trading-engine URL to test has not been set."
   exit 1
 else
-  echo -e "The Command-side URL for this smoke test is: \e[32m $engineURL \e[0m"
+  echo -e "The Trading-engine URL for this smoke test is: \e[32m $engineURL \e[0m"
 fi
 
 if [ -z $appURL ];
 then
-  echo -e "\e[31mThe Query-side URL to test has not been set."
+  echo -e "\e[31mThe Trader-app URL to test has not been set."
   exit 1
 else
-  echo -e "The Query-side URL for this smoke test is: \e[32m $appURL \e[0m"
+  echo -e "The Trader-app URL for this smoke test is: \e[32m $appURL \e[0m"
 fi
 
 # Make sure the homepage shows...
@@ -26,7 +26,7 @@ if curl -sL -w %{http_code} "$engineURL/info" -o /dev/null | grep "200"
 then
     echo "[$engineURL/info] shows 'HTTP/1.1 200 OK' (as expected)."
 else
-    echo -e "\e[31mError. Command-side unresponsive. Failed to show '200 OK' on [$engineURL/info]"
+    echo -e "\e[31mError. Trading-engine is unresponsive. Failed to show '200 OK' on [$engineURL/info]"
     exit 1
 fi
 
@@ -34,7 +34,7 @@ if curl -sL -w %{http_code} "$appURL/info" -o /dev/null | grep "200"
 then
     echo "[$appURL/info] shows 'HTTP/1.1 200 OK' (Expected)."
 else
-    echo -e "\e[31mError. Query-side unresponsive. Failed to show '200 OK' on [$appURL/info]"
+    echo -e "\e[31mError. Trader-app is unresponsive. Failed to show '200 OK' on [$appURL/info]"
     exit 1
 fi
 
