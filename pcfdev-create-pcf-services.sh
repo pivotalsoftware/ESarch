a9fa0ba877afbd40fd53be813f1b53c9d723cd2c
benwilcock <benwilcock@gmail.com>
2018-07-26 16:38:49 +0100

    tidied up some old files

--- pcfdev-create-pcf-services.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cf create-service p-mysql 512mb mysql
-cf create-service p-rabbitmq standard rabbit
-cf create-service p-service-registry standard registry
-cf create-service p-config-server standard config -c config-server-setup.json
