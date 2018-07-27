cd31452efee1733567f8171444ada97734254c95
benwilcock <benwilcock@gmail.com>
2018-07-27 11:29:26 +0100

    Added a few lines for cf add-network-policy Added health checks to the manifests

--- pws-create-pcf-services.sh
+++ pws-create-pcf-services.sh
@@ -4,4 +4,6 @@ cf create-service cleardb spark mysql
 cf create-service cloudamqp lemur rabbit
 cf create-service p-service-registry standard registry
 cf create-service p-config-server standard config -c '{"git": { "uri": "https://github.com/dcaron/esrefarch-config" } }'
+cf add-network-policy esrefarch-demo-trader-app --destination-app esrefarch-demo-trading-engine
+cf add-network-policy esrefarch-demo-trading-engine --destination-app esrefarch-demo-trader-app
 
