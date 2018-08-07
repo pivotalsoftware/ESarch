0ceb325ede46f7ebc578537cce32bc2d2b01b892
Allard Buijze <allard.buijze@axoniq.io>
2018-08-07 21:50:02 +0200

    Added actuator endpoint to expose distributed command bus status

---
+++ trading-engine/src/main/java/io/pivotal/refarch/cqrs/trader/tradingengine/DistributedCommandBusActuator.java
@@ -0,0 +1,43 @@
+package io.pivotal.refarch.cqrs.trader.tradingengine;
+
+import org.axonframework.commandhandling.distributed.CommandRouter;
+import org.axonframework.commandhandling.distributed.ConsistentHash;
+import org.axonframework.commandhandling.distributed.ConsistentHashChangeListener;
+import org.axonframework.commandhandling.distributed.RoutingStrategy;
+import org.axonframework.springcloud.commandhandling.SpringCloudCommandRouter;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
+
+@Component
+@Endpoint(id = "distributed-command-bus")
+public class DistributedCommandBusActuator {
+
+    private final AtomicReference<ConsistentHash> consistentHash = new AtomicReference<>();
+
+    @ReadOperation
+    public String consistentHash() {
+        return consistentHash.get().getMembers().toString();
+    }
+
+    @Bean
+    public ConsistentHashChangeListener consistentHashChangeListener() {
+        return consistentHash::set;
+    }
+
+    @Bean
+    public CommandRouter springCloudCommandRouter(DiscoveryClient discoveryClient,
+                                                  Registration localServiceInstance,
+                                                  RoutingStrategy routingStrategy,
+                                                  ConsistentHashChangeListener chcListener) {
+        return new SpringCloudCommandRouter(discoveryClient, localServiceInstance, routingStrategy, Objects::nonNull,
+                                            chcListener);
+    }
+
+}
