8a31b229bd4db882e1344017abf90003e621ab7e
smcvb <smcvbeelen@gmail.com>
2018-08-08 20:29:02 +0200

    Make TransactionState a Kotlin enum to fit with the rest
    Make TransactionState a Kotlin enum to fit with the rest

--- trader-app/src/main/java/io/pivotal/refarch/cqrs/trader/coreapi/orders/transaction/valueObjects.kt
+++ trader-app/src/main/java/io/pivotal/refarch/cqrs/trader/coreapi/orders/transaction/valueObjects.kt
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.annotation.JsonValue
 import org.axonframework.common.IdentifierFactory
 import java.io.Serializable
 
-
 data class TransactionId(@JsonValue val identifier: String = IdentifierFactory.getInstance().generateIdentifier()) : Serializable {
 
     companion object {
         private const val serialVersionUID = -5267104328616955617L
     }
 
+}
+
+enum class TransactionState {
+    STARTED, CONFIRMED, CANCELLED, EXECUTED, PARTIALLY_EXECUTED
 }
