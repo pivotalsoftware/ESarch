d5e5deec7654c24e948397b83cdb027c397c82fa
benwilcock <benwilcock@gmail.com>
2018-08-09 20:55:07 +0100

    Added random values to the contract spec

--- trader-app/src/test/resources/contracts/trader-ui/shouldCreateNewCompany.groovy
+++ trader-app/src/test/resources/contracts/trader-ui/shouldCreateNewCompany.groovy
@@ -7,15 +7,14 @@ Contract.make {
     request {
         method 'POST'
         url "/command/CreateCompanyCommand"
-        body """
-            {
-            "companyId":"7962b5b3-f223-4c2e-aadf-0e908f2331b1",
-            "userId":"b1469bc0-3e04-4c3a-8d18-24d5df513a0a",
-            "companyName":"COMPANY-cf2d1993-0380-4f5e-94ef-09cdd1fd222c",
-            "companyValue":0,
-            "amountOfShares":0
-            }
-        """
+        body (
+                "companyId" : anyUuid(),
+                "userId" : anyNonBlankString(),
+                "companyName" : anyNonEmptyString(),
+                "companyValue" : anyPositiveInt(),
+                "amountOfShares" : anyPositiveInt()
+        )
+
         headers {
             contentType applicationJson()
         }
