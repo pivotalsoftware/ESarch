ecf07d3b95e4d4a39c7b6420b69838bfad362d30
benwilcock <benwilcock@gmail.com>
2018-08-10 17:03:22 +0100

    removed the jar_count check performed by the CI build, as there are now 2 jars, one for the app and one for the stubs

--- ci/tasks/build-trader-app.sh
+++ ci/tasks/build-trader-app.sh
@@ -10,14 +10,16 @@ cd source-code/trader-app
   mvn verify
 cd $FOLDER
 
-jar_count=`find source-code/trader-app/target -type f -name *.jar | wc -l`
+# jar_count=`find source-code/trader-app/target -type f -name *.jar | wc -l`
 
-if [ $jar_count -gt 1 ]; then
-  echo "More than one jar found, don't know which one to deploy. Exiting :("
-  exit 1
-fi
+# if [ $jar_count -gt 1 ]; then
+#   echo "More than one jar found, don't know which one to deploy. Exiting :("
+#   exit 1
+# fi
 
-find source-code/trader-app/target -type f -name *.jar -exec cp "{}" package-output/trader-app.jar \;
+# find source-code/trader-app/target -type f -name *.jar -exec cp "{}" package-output/trader-app.jar \;
+
+find source-code/trader-app/target -type f -name trader-app.jar -exec cp "{}" package-output/trader-app.jar \;
 
 echo "Done packaging"
 exit 0
