a9953a38d0a1bb6c8e7ddaa152b181287ae0bc34
smcvb <smcvbeelen@gmail.com>
2018-08-21 19:48:10 +0200

    Initial SpringCloudContact adjustments
    -Make some minor adjustments to the BaseContractTest
    -Adjust directories structure of SCC's
    -Move shouldCreateNewCompany.groovy to new directory
    -Reindent file, start of trying to assert returned UUID

--- trader-app/src/test/java/io/pivotal/refarch/cqrs/trader/app/BaseContractTest.java
+++ trader-app/src/test/java/io/pivotal/refarch/cqrs/trader/app/BaseContractTest.java
@@ -5,30 +5,35 @@ import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
 import io.pivotal.refarch.cqrs.trader.app.controller.CommandController;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.axonframework.commandhandling.gateway.CommandGateway;
-import org.junit.Before;
+import org.junit.*;
+import org.springframework.http.ResponseEntity;
 
 import java.util.concurrent.CompletableFuture;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class BaseContractTest {
 
-  @Before
-  public void setup(){
+    private static final String EXPECTED_UUID = "expected-uuid";
 
-    final CommandGateway commandGateway = mock(CommandGateway.class);
-    final CompletableFuture<Object> mockCompletableFuture = mock(CompletableFuture.class);
+    @SuppressWarnings("unchecked")
+    @Before
+    public void setup() {
+        final CommandGateway commandGateway = mock(CommandGateway.class);
+        final CompletableFuture<Object> mockCompletableFuture = mock(CompletableFuture.class);
 
-    final ObjectMapper objectMapper = new ObjectMapper();
-    final JsonSchemaGenerator jsonSchemaGenerator = new JsonSchemaGenerator(objectMapper);
+        when(mockCompletableFuture.thenApply(ResponseEntity::ok))
+                .thenReturn(CompletableFuture.completedFuture(ResponseEntity.ok(EXPECTED_UUID)));
+        when(commandGateway.send(any())).thenReturn(mockCompletableFuture);
 
-    final CommandController commandController = new CommandController(commandGateway,objectMapper,jsonSchemaGenerator);
-    final ClassLoader classLoader = this.getClass().getClassLoader();
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final JsonSchemaGenerator jsonSchemaGenerator = new JsonSchemaGenerator(objectMapper);
 
-    commandController.setBeanClassLoader(classLoader);
-    when(commandGateway.send(any())).thenReturn(mockCompletableFuture);
-    RestAssuredMockMvc.standaloneSetup(commandController);
-  }
+        final CommandController commandController =
+                new CommandController(commandGateway, objectMapper, jsonSchemaGenerator);
+        commandController.setBeanClassLoader(this.getClass().getClassLoader());
+
+        RestAssuredMockMvc.standaloneSetup(commandController);
+    }
 }
