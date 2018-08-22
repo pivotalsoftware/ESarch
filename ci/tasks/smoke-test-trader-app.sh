7e14d93bbd02c3ed8f782c32ff33797f06b66f08
smcvb <smcvbeelen@gmail.com>
2018-08-22 12:15:19 +0200

    Remove Company and OrderBook controllers
    -Remove CompanyController as the CommandController does its job
    -Remove OrderBookController as the Command- and QueryController do its
    job
    -Adjust http-api.http to not use the Company- and OrderBookController
    anymore
    -Adjust smoke-test-trader-app.sh to not use the Company- and
    OrderBookController anymore, but use the new command and query endpoints

--- ci/tasks/smoke-test-trader-app.sh
+++ ci/tasks/smoke-test-trader-app.sh
@@ -9,7 +9,7 @@ echo "Smoke Testing the Trader App using the URL: ${URL}"
 # Begin the Smoke-testing...
 
 export HEALTH_STATUS=`curl -sL -X GET ${URL}/actuator/health | jq -r .status`
-if [ -z $HEALTH_STATUS ] || [ "$HEALTH_STATUS" != "UP" ]
+if [ -z ${HEALTH_STATUS} ] || [ "$HEALTH_STATUS" != "UP" ]
 then
     echo -e "\e[31mError. The smoke test has failed, the application health check didn't work!"
     exit 1
@@ -18,7 +18,7 @@ else
 fi
 
 export API=`curl -sL -X GET ${URL}/command/api | jq '. | length'`
-if [ -z $API ] || [[ $API < 1 ]]
+if [ -z ${API} ] || [[ ${API} < 1 ]]
 then
     echo -e "\e[31mError. The smoke test has failed, the application API command count was too low!"
     exit 1
@@ -26,22 +26,25 @@ else
     echo "The API check status is reporting that there are ${API} commands available."
 fi
 
-export RANDOM_NAME=`uuidgen`
-echo "The randomly generated Company Name is: ${RANDOM_NAME}"
+export RANDOM_COMPANY_ID=`uuidgen`
+echo "The randomly generated Company Id is: ${RANDOM_COMPANY_ID}"
+export RANDOM_COMPANY_NAME=`uuidgen`
+echo "The randomly generated Company Name is: ${RANDOM_COMPANY_NAME}"
+export CREATE_COMPANY_PAYLOAD="{\"companyId\": \"${RANDOM_COMPANY_ID}\", \"userId\": \"1bae0365-949c-467d-a6f5-4a32ddd96dbb\", \"companyName\": \"${RANDOM_COMPANY_NAME}\", \"companyValue\": \"1337\", \"amountOfShares\": \"42\"}"
 
-export COMPANY_UUID=`curl -X POST -sL -d "${RANDOM_NAME}" -H "Content-Type:application/json" ${URL}/company | jq -r ''`
-if [ -z $COMPANY_UUID ] || [ "$COMPANY_UUID" = "" ];
+export COMPANY_UUID=`curl -X POST -sL -d "${CREATE_COMPANY_PAYLOAD}" -H "Content-Type:application/json" ${URL}/command/CreateCompanyCommand | jq -r ''`
+if [ -z ${COMPANY_UUID} ] || [ "$COMPANY_UUID" = "" ];
 then
     echo -e "\e[31mError. The smoke test has failed, it didn't create a new company!"
     exit 1
 else
-    echo "The company (${RANDOM_NAME}) was created and assigned a UUID of: ${COMPANY_UUID} by the application"
+    echo "The company (${RANDOM_COMPANY_NAME}) was created, with a UUID of [${COMPANY_UUID}]."
 fi
 
-export COMPANY_NAME=`curl -sL -H "Content-Type: application/json" -X GET ${URL}/orderbook/${COMPANY_UUID} | jq -r '.[]|.companyName'`
+export COMPANY_NAME=`curl -sL -H "Content-Type: application/json" -X GET ${URL}/query/order-book/by-company/${COMPANY_UUID} | jq -r '.[]|.companyName'`
 echo "The Company Name  is: ${COMPANY_NAME}"
 
-if [ "$COMPANY_NAME" != "$RANDOM_NAME" ];
+if [ "$COMPANY_NAME" != "$RANDOM_COMPANY_NAME" ];
 then
     echo -e "\e[31mError. The smoke test has failed, it was unable to find the orderbook for the company with the company-id [$COMPANY_UUID]"
     exit 1
