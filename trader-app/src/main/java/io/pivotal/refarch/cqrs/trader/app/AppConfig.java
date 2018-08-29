b8738f016eb31dff0be325286a9dae80d032e86d
smcvb <smcvbeelen@gmail.com>
2018-08-29 19:13:49 +0200

    Add the LoggingInterceptor to the localSegment
    Add the LoggingInterceptor to the localSegment, so that we have info log
     statements of every command message being published.

--- trader-app/src/main/java/io/pivotal/refarch/cqrs/trader/app/AppConfig.java
+++ trader-app/src/main/java/io/pivotal/refarch/cqrs/trader/app/AppConfig.java
@@ -2,13 +2,30 @@ package io.pivotal.refarch.cqrs.trader.app;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
+import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.commandhandling.distributed.DistributedCommandBus;
+import org.axonframework.messaging.interceptors.LoggingInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    /**
+     * We have chosen to take this <i>optional</i> step in order to benefit from automatic logging of every command
+     * message being published on to the {@link CommandBus}. These are `INFO` level statements of every action.
+     * We have qualified the given CommandBus parameter with 'localSegment' as we are using a
+     * {@link DistributedCommandBus} and we want to associate this {@link LoggingInterceptor} with the local CommandBus
+     * node.
+     *
+     * @param commandBus the {@link CommandBus} auto configured for this application
+     */
+    @Autowired
+    public void configure(@Qualifier("localSegment") CommandBus commandBus) {
+        commandBus.registerHandlerInterceptor(new LoggingInterceptor<>());
+    }
 
     @Autowired
     public void configure(DistributedCommandBus commandBus) {
