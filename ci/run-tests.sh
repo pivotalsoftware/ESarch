9d0067baf71e40e81c6ba2ede610a30c854ac77a
benwilcock <benwilcock@gmail.com>
2018-09-13 19:25:11 +0100

    added a e2e test that will call the data initializer after a fresh deployment of the trader app

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -14,6 +14,12 @@ then
     export appURL=https://esrefarch-demo-trader-app.cfapps.io
 fi
 
+if [ -n $uiURL ]
+then
+    echo -e "Setting a default URL for the Trader UI..."
+    export uiURL=https://esrefarch-demo-trader-ui.cfapps.io
+fi
+
 env URL="$engineURL" ./tasks/smoke-test-trading-engine.sh
 env URL="$appURL" ./tasks/smoke-test-trader-app.sh
 ./tasks/e2e-test-both-sides.sh
