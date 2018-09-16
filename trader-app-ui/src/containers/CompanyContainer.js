c62f4f0c89c4bafcad733e138b9ee06c02f98f09
Sampath Kunta <skunta@solstice-consulting.com>
2018-09-16 10:23:01 -0500

    TraderAppUI - commented out sse code

--- trader-app-ui/src/containers/CompanyContainer.js
+++ trader-app-ui/src/containers/CompanyContainer.js
@@ -35,14 +35,14 @@ class CompanyContainer extends Component {
     if(oldProps.tradeDetails.orderBook.isFetching && !this.props.tradeDetails.orderBook.isFetching
         && !this.props.tradeDetails.orderBook.error) {
           const orderBookId = this.props.tradeDetails.orderBook.identifier;
-          this.state.eventSource = new EventSource(`${API_ROOT}/query/subscribe/order-book/${orderBookId}`);
-          this.state.eventSource.onmessage = (event) => {
-            if(event.data) {
-              let jsonData = JSON.parse(event.data);
-              console.log('event json data', jsonData);
-              this.props.companyActions.setSSEOrderBookData(jsonData);
-            }
-          }
+        //   this.state.eventSource = new EventSource(`${API_ROOT}/query/subscribe/order-book/${orderBookId}`);
+        //   this.state.eventSource.onmessage = (event) => {
+        //     if(event.data) {
+        //       let jsonData = JSON.parse(event.data);
+        //       console.log('event json data', jsonData);
+        //       this.props.companyActions.setSSEOrderBookData(jsonData);
+        //     }
+        //   }
         }
   }
 
