b52a75a4a9e848eef9bf39481f8c465e6fd506c1
Sampath Kunta <skunta@solstice-consulting.com>
2018-09-19 14:06:26 -0500

    Implement CI/CD for ReactApp

--- trader-app-ui/src/components/Home/index.js
+++ trader-app-ui/src/components/Home/index.js
@@ -22,7 +22,7 @@ class Home extends Component {
         <div className="container">
           <div className="row align-items-center">
             <div className="col-sm-6 px-0">
-              <img src={require('../../assets/home-foreground-image.png')}
+              <img alt="home background" src={require('../../assets/home-foreground-image.png')}
                    style={{width: '100%', height: '100%'}}/>
             </div>
             <div className="col-sm-6 mt-2 pt-5 text-right">
