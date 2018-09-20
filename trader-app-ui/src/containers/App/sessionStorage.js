74ddc89699b6da7065e687b611542b387dee2a88
Haridu Senadeera <hsenadeera@solstice.com>
2018-09-20 11:50:15 -0500

    persist portfolio id in sessionStorage
    fixes issues where buy and sell orders werent being placed after refreshing the page

--- trader-app-ui/src/containers/App/sessionStorage.js
+++ trader-app-ui/src/containers/App/sessionStorage.js
@@ -1,13 +1,21 @@
 export const loadState = () => {
   try {
-    const serializedState = sessionStorage.getItem('axonState');
-    if(serializedState == null) {
+    const serializedHomeState = sessionStorage.getItem('axonHomeState');
+    const serializedPortfolioState = sessionStorage.getItem('axonPortfolioState');
+    let initialState = {};
+
+    if(serializedHomeState == null && serializedPortfolioState== null) {
       return undefined;
     }
-    const homeState = JSON.parse(serializedState);
-    return {
-      home: homeState
+
+    if(serializedHomeState) {
+      initialState.home = JSON.parse(serializedHomeState);
     }
+
+    if(serializedPortfolioState) {
+      initialState.portfolio = JSON.parse(serializedPortfolioState);
+    }
+    return initialState;
   } catch(err) {
     return undefined;
   }
@@ -16,8 +24,12 @@ export const loadState = () => {
 export const saveState = (state) => {
   try {
     // save state.home (authentication info) in sessionStorage
-    const serializedState = JSON.stringify(state.home);
-    sessionStorage.setItem('axonState', serializedState);
+    const serializedHomeState = JSON.stringify(state.home);
+    sessionStorage.setItem('axonHomeState', serializedHomeState);
+
+    const serializedPortfolioState = JSON.stringify(state.portfolio);
+    sessionStorage.setItem('axonPortfolioState', serializedPortfolioState);
+
   } catch (err) {
     // ignore write errors
   }
