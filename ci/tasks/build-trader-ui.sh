c3e4385d73222c30b7a5685eea5bbdaa94012f72
benwilcock <benwilcock@gmail.com>
2018-09-20 21:57:59 +0100

    experimenting to try and fix issues with node.js build

--- ci/tasks/build-trader-ui.sh
+++ ci/tasks/build-trader-ui.sh
@@ -15,10 +15,7 @@ cd source-code/trader-app-ui
 cd $FOLDER
 
 cp -R source-code/trader-app-ui/build package-output
-
-ls -la source-code/trader-app-ui
 ls -la package-output/build
-ls -la package-output/build/static
 
 echo "Done building trader ui"
 exit 0
