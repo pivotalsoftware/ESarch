604efce63674de0b1cfd9460fca58d35a198512f
benwilcock <benwilcock@gmail.com>
2018-09-20 21:14:19 +0100

    experimenting to try and fix issues with node.js build

--- ci/tasks/build-trader-ui.sh
+++ ci/tasks/build-trader-ui.sh
@@ -9,15 +9,11 @@ echo "Packaging the Trader UI App..."
 cd source-code/trader-app-ui
   npm install
   npm run build
+  # copy the build folder to package-output
+  cp -R ./build package-output/build
 cd $FOLDER
 
-# copy the build folder to package-output
-cp -r source-code/trader-app-ui/build package-output/build
-cp -r source-code/trader-app-ui/manifest.yml package-output/build/manifest.yml
-
-ls -la source-code
 ls -la source-code/trader-app-ui
-ls -la package-output
 ls -la package-output/build
 
 echo "Done building trader ui"
