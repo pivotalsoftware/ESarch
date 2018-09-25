f8be4cea43351797980aa921094a61a8c5597ad9
Sampath Kunta <skunta@solstice-consulting.com>
2018-09-25 18:44:25 -0400

    Add axontrader.cfapps.io hostname so it points to the right backend url

--- trader-app-ui/src/utils/config.js
+++ trader-app-ui/src/utils/config.js
@@ -1,6 +1,7 @@
 export const HostURLsMapping = {
     "localhost": "http://localhost:8080",
     "esrefarch-demo-trader-ui.cfapps.io": "https://esrefarch-demo-trader-app.cfapps.io",
+    "axontrader.cfapps.io": "https://esrefarch-demo-trader-app.cfapps.io",
 }
 export const ApiConfig = () => {
     let hostname = window.location.hostname;
