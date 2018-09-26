eca7bb60d6de77e743a05b7cb925574a694bdb69
Haridu Senadeera <hsenadeera@solstice.com>
2018-09-26 10:16:00 -0500

    fixed homepage paragraph links styling

--- trader-app-ui/src/components/Home/Banner.js
+++ trader-app-ui/src/components/Home/Banner.js
@@ -6,7 +6,13 @@ const Banner = () => {
     <div>
       <h1 className="homepage-heading">The Trader</h1>
       <p className="text-homepage">
-        Welcome to the Axon Trader application. Axon is the leading open source CQRS and Event Sourcing framework for <a href="http://projects.spring.io/spring-boot">Spring Boot</a>. Axon Trader showcases various <a href="http://www.axonframework.org/">Axon Framework</a> capabilities and it is built for <a href="https://pivotal.io/platform/pivotal-application-service">Pivotal Application Service</a> (provided by <a href="https://run.pivotal.io">Pivotal Web Services</a>). To get the code checkout <a href="https://github.com/pivotalsoftware/ESarch">this repository</a>.
+        Welcome to the Axon Trader application.
+        Axon is the leading open source CQRS and Event Sourcing framework for
+        &nbsp;<a href="http://projects.spring.io/spring-boot" className="homepage-link" target="_blank" rel="noopener noreferrer">Spring Boot</a>.
+        &nbsp;Axon Trader showcases various <a href="http://www.axonframework.org/" className="homepage-link" target="_blank" rel="noopener noreferrer">Axon Framework</a>
+        &nbsp;capabilities and it is built for <a href="https://pivotal.io/platform/pivotal-application-service" className="homepage-link" target="_blank" rel="noopener noreferrer">Pivotal Application Service</a> 
+        &nbsp;(provided by <a href="https://run.pivotal.io" className="homepage-link" target="_blank" rel="noopener noreferrer">Pivotal Web Services</a>). 
+        To get the code checkout <a href="https://github.com/pivotalsoftware/ESarch" className="homepage-link" target="_blank" rel="noopener noreferrer">this repository</a>.
         </p>
       <p className="text-info-homepage banner-info">
         Choose a user to impersonate below, then go to your account dashboard.
